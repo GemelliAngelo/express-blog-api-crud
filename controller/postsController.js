4d7b6e8e82c7449c7ae4f0bc994cd059c5c36719
GemelliAngelo <gemeangelo27@gmail.com>
2024-11-21 12:59:41 +0100

    add controllers

---
+++ controller/postsController.js
@@ -0,0 +1,45 @@
+// # index
+function index(req, res) {
+  res.json(postsData);
+}
+
+// # show
+function show(req, res) {
+  const id = parseInt(req.params.id);
+
+  if (isNaN(id)) {
+    res.status(418).send("id not valid");
+    return;
+  }
+
+  if (id > postsData.length - 1 || id < 0) {
+    res.status(404).send("id not found");
+  }
+
+  res.json(postsData[id]);
+}
+
+// # store
+function store(req, res) {
+  res.send(`Aggiunta di un post`);
+}
+
+// # update
+function update(req, res) {
+  const { id } = req.params;
+  res.send(`Modifica integrale del post con indice ${id}`);
+}
+
+// # modify
+function modify(req, res) {
+  const { id } = req.params;
+  res.send(`Modifica parziale del post con indice ${id}`);
+}
+
+// # destroy
+function destroy(req, res) {
+  const { id } = req.params;
+  res.send(`Eliminazione del post con indice ${id}`);
+}
+
+module.exports = { index, show, store, update, modify, destroy };
--- routers/posts.js
+++ routers/posts.js
@@ -1,50 +1,15 @@
+// # Raccolta dati
 const express = require("express");
 const router = express.Router();
 const postsData = require("../data/postsData.js");
+const postsController = require("../controller/postsController.js");
 
-// * INDEX
-router.get("/", (req, res) => {
-  res.json(postsData);
-});
-
-// * SHOW
-router.get("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-
-  if (isNaN(id)) {
-    res.status(418).send("id not valid");
-    return;
-  }
-
-  if (id > postsData.length - 1 || id < 0) {
-    res.status(404).send("id not found");
-  }
-
-  res.json(postsData[id]);
-});
-
-// * STORE
-router.post("/", (req, res) => {
-  res.send(`Aggiunta di un post`);
-});
-
-// * UPDATE
-router.put("/:id", (req, res) => {
-  const { id } = req.params;
-  res.send(`Modifica integrale del post con indice ${id}`);
-});
-
-// * MODIFY
-router.patch("/:id", (req, res) => {
-  const { id } = req.params;
-  res.send(`Modifica parziale del post con indice ${id}`);
-});
-
-// * DESTROY
-
-router.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  res.send(`Eliminazione del post con indice ${id}`);
-});
+// # Routers
+router.get("/", postsController.index);
+router.get("/:id", postsController.show);
+router.post("/", postsController.show);
+router.put("/:id", postsController.update);
+router.patch("/:id", postsController.modify);
+router.delete("/:id", postsController.destroy);
 
 module.exports = router;
