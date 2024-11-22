c336a3d9571419b8b0c94d5f7c958f806c504819
GemelliAngelo <gemeangelo27@gmail.com>
2024-11-22 14:40:55 +0100

    add update function

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -19,46 +19,77 @@ function show(req, res) {
   const id = parseInt(req.params.id);
 
   if (isNaN(id)) {
-    return res.status(418).send("id not valid");
+    return res.status(400).send({ error: "id not valid" });
   }
 
   if (id > postsData.length - 1 || id < 0) {
-    return res.status(404).send("id not found");
+    return res.status(400).send({ error: "id not found" });
   }
 
   const post = postsData.find((post) => post.id === id);
+
+  if (!post) {
+    res.status(400).send({ error: "Data not found" });
+  }
+
   res.json(post);
 }
 
 // # STORE
 function store(req, res) {
   const id = postsData.at(-1).id + 1;
+  const { title, content, image, tags } = req.body;
 
-  if (
-    !req.body.title ||
-    !req.body.content ||
-    !req.body.image ||
-    !req.body.tags?.length
-  ) {
+  if (!title || !content || !image || !tags?.length) {
     return res.status(400).send({ error: "Missing data not found" });
   }
 
   const newPost = {
     id: id,
-    title: req.body.title,
-    content: req.body.content,
-    image: req.body.image,
-    tags: req.body.tags,
+    title: title,
+    content: content,
+    image: image,
+    tags: tags,
   };
 
   postsData.push(newPost);
-  res.json(postsData);
+  console.log(postsData);
+  res.sendStatus(204);
 }
 
 // # UPDATE
 function update(req, res) {
-  const { id } = req.params;
-  res.send(`Modifica integrale del post con indice ${id}`);
+  const id = parseInt(req.params.id);
+
+  if (isNaN(id)) {
+    return res.status(418).send({ error: "id not valid" });
+  }
+
+  if (id > postsData.length - 1 || id < 0) {
+    return res.status(400).send({ error: "id not found" });
+  }
+
+  let post = postsData.find((post) => post.id === id);
+
+  if (!post) {
+    res.status(400).send({ error: "Data not found" });
+  }
+
+  const { title, content, image, tags } = req.body;
+
+  if (!title || !content || !image || !tags?.length) {
+    return res.status(400).send({ error: "Missing data not found" });
+  }
+
+  (post.id = id),
+    (post.title = title),
+    (post.content = content),
+    (post.image = image),
+    (post.tags = tags);
+
+  console.log(postsData);
+
+  res.sendStatus(204);
 }
 
 // # MODIFY
@@ -72,18 +103,18 @@ function destroy(req, res) {
   const id = parseInt(req.params.id);
 
   if (isNaN(id)) {
-    return res.status(418).send({ error: "id not valid" });
+    return res.status(400).send({ error: "id not valid" });
   }
 
   if (id < 0) {
-    return res.status(404).send({ error: "id not found" });
+    return res.status(400).send({ error: "id not found" });
   }
 
   const deletedPost = postsData.find((post) => post.id === id);
   const postIndex = postsData.indexOf(deletedPost);
 
   if (postIndex === -1) {
-    return res.status(404).send({ error: "id not found" });
+    return res.status(400).send({ error: "id not found" });
   }
 
   postsData.splice(postIndex, 1);
